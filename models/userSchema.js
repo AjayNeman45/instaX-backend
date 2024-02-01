f6f166f133136aba591135d44fc63920ee84aa86
mirats-sde <126751395+ajayneman@users.noreply.github.com>
2024-02-01 23:58:55 +0530

    changes

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,33 +2,36 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true,
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+		},
+		profilePhoto: {
+			type: String,
+			required: true,
+		},
 	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	name: {
-		type: String,
-		required: true,
-	},
-	description: {
-		type: String,
-	},
-	profilePhoto: {
-		type: String,
-		required: true,
-	},
-})
+	{ timestamps: true }
+)
 
 const user = mongoose.model("user", userSchema)
 
